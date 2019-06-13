cb49069c52edf0959f00b37ad4598b3e9cceb6ad
adarshmelethil <adarshmelethil@gmail.com>
2019-06-13 15:44:14 -0600

    Updates

---
+++ precache-manifest.1602abb3f717beb4a1f1a5a13dc82a05.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "563ec866ed6c86ca256152674b92db75",
+    "url": "/MoneyWatch/index.html"
+  },
+  {
+    "revision": "31d706318aaf852bb834",
+    "url": "/MoneyWatch/static/css/2.d6d7c54f.chunk.css"
+  },
+  {
+    "revision": "516ace27096d225cacc8",
+    "url": "/MoneyWatch/static/css/main.0fd6dca5.chunk.css"
+  },
+  {
+    "revision": "31d706318aaf852bb834",
+    "url": "/MoneyWatch/static/js/2.b1e19024.chunk.js"
+  },
+  {
+    "revision": "516ace27096d225cacc8",
+    "url": "/MoneyWatch/static/js/main.e0ee1e20.chunk.js"
+  },
+  {
+    "revision": "ac629df03023fa7c1e21",
+    "url": "/MoneyWatch/static/js/runtime~main.15963d96.js"
+  },
+  {
+    "revision": "160851ae834db92faabc80a6f8a6165e",
+    "url": "/MoneyWatch/static/media/DrumpTonald.160851ae.png"
+  },
+  {
+    "revision": "5ecfe1bfbe24aed25449cab5b048095f",
+    "url": "/MoneyWatch/static/media/InterviewProof.5ecfe1bf.JPG"
+  },
+  {
+    "revision": "60337e2359321ac515ac1711b3e5affe",
+    "url": "/MoneyWatch/static/media/JeanneChapdelaine.60337e23.png"
+  },
+  {
+    "revision": "d6848ab03c938b3aa6eaf5634adef0cd",
+    "url": "/MoneyWatch/static/media/MoneyWatchFinal1.d6848ab0.pdf"
+  },
+  {
+    "revision": "a032df75245db82f06664ac63e5e3298",
+    "url": "/MoneyWatch/static/media/SteveDaniel.a032df75.png"
+  },
+  {
+    "revision": "1bd52a8206d27c8a5709ee11d3818264",
+    "url": "/MoneyWatch/static/media/UmairRaja.1bd52a82.png"
+  }
+]);
